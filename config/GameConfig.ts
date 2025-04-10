e4c47c4176bc105626fc7b518396af96c6d622c5
woodRock <j.r.h.wood98@gmail.com>
2025-04-11 01:41:58 +1200

    Collisions work; transitions work

--- config/GameConfig.ts
+++ config/GameConfig.ts
@@ -2,7 +2,7 @@ export const GameConfig = {
   cubeSize: 50,
   mazeSize: 25,
   playerRadius: 0.5,
-  playerSpeed: 10.0,
+  playerSpeed: 5,
   acceleration: 0.05,
   deceleration: 0.85,
   jumpForce: 0.2,
